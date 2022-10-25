c49db7171dca7b43e6e449ab4b35aa1ebef3f11a
Василий Петухов <vonono.weewsa@yandex.ru>
2022-10-25 16:18:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp241022/Logic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp241022
+{
+    internal class Logic
+    {
+        public bool IsNumbersDifferenceCorrect(List<int> numbers)
+        {
+            bool lastF = numbers[1] - numbers[0] >= 0;
+
+            for (int i = 2; i < numbers.Count; i++)
+            {
+                if ((numbers[i] - numbers[i-1] >= 0 && !lastF) || (numbers[i] - numbers[i - 1] < 0 && lastF))
+                {
+                    lastF = !lastF;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void NumbersDifferenceCorrectWithChanges(ref List<int> numbers)
+        {
+            bool lastF = numbers[1] - numbers[0] >= 0;
+
+            for (int i = 2; i < numbers.Count; i++)
+            {
+                if ((numbers[i] - numbers[i - 1] >= 0 && !lastF) || (numbers[i] - numbers[i - 1] < 0 && lastF))
+                {
+                    lastF = !lastF;
+                }
+                else
+                {
+                    numbers.RemoveAt(i);
+
+                    Console.WriteLine();
+                    numbers.ForEach((s) => Console.Write(s + ", "));
+
+                    NumbersDifferenceCorrectWithChanges(ref numbers);
+
+                    break;
+                }
+            }
+
+            if (numbers.Count == 2)
+            {
+                numbers.Clear();
+            }
+        }
+    }
+}
---
+++ ConsoleApp241022/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp241022;
+
+int n = int.Parse(Console.ReadLine());
+
+List<int> numbers = new List<int>(n);
+
+string[] strs = Console.ReadLine().Split(new char[] { ' ' });
+
+for (int i = 0; i < strs.Length; i++)
+{
+    numbers.Add(int.Parse(strs[i]));
+}
+
+Logic logic = new Logic();
+
+logic.NumbersDifferenceCorrectWithChanges(ref numbers);
+
+//numbers.ForEach((s) => Console.WriteLine(s));
+Console.WriteLine("\nИтог " + numbers.Count);
+
+Console.ReadKey();
